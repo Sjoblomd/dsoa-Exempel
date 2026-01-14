36821e3122122a1f3e105a7831e892db61a5c2db
sjoblomd <sjoblomd@arcada.fi>
2026-01-14 15:40:22 +0200

    Lektion1 / Car class with setter and getter

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Car car = new Car("Volkswagen");
+        Car car2 = new Car("BMW");
+
+        System.out.println(car.getName());
+
+        car.setName("Audi");
+        System.out.println(car.getName());
+        System.out.println(car2.getName());
     }
 }
